5b5baad7ea4b4b0223f0b02443a09e0e78e3b07c
delliCC <desarrollo.web.chontalpa@gmail.com>
2022-09-09 17:18:41 -0500

    configurando axios

--- src/components/ProjectsTwo/Protfolio.vue
+++ src/components/ProjectsTwo/Protfolio.vue
@@ -8,6 +8,8 @@
             </div>
 
             <div class="row">
+                // eslint-disable-next-line vue/require-v-for-key
+                <span v-for="estudios in array_estudios" :key=""></span>
                 <div class="col-lg-4 col-md-6 col-sm-6">
                     <div class="single-protfolio">
                         <div class="image">
@@ -27,7 +29,7 @@
                     </div>
                 </div>
 
-                <div class="col-lg-4 col-md-6 col-sm-6">
+                <!-- <div class="col-lg-4 col-md-6 col-sm-6">
                     <div class="single-protfolio">
                         <div class="image">
                             <router-link to="/estudios-detalle">
@@ -120,7 +122,7 @@
                             </router-link>
                         </div>
                     </div>
-                </div>
+                </div> -->
 
                 <div class="col-lg-12 col-md-12">
                     <div class="pagination-area">
@@ -147,41 +149,30 @@
         name: 'Protfolio',
         data() {
             return {
-                
+                array_estudios:[]
             }
         },
         mounted() {
             this.estudios()
         },
         methods:{
-            estudios(ctx, callback) {
-                this.$axios.get('http://localhost/api/estudios', {
-                    // params: {
-                    // page: ctx.currentPage,
-                    // filter: ctx.filter,
-                    // sortBy: ctx.sortBy,
-                    // sortDesc: ctx.sortDesc ? 'desc' : 'asc',
-                    // perPage: ctx.perPage,
-                    // },
-                }).then(response => {
-                    console.log(response)
-                    const res = response.data.data
-                    console.log(res)
-                    // const clientes = res.data.map(element => ({
-                    // id: element.id,
-                    // ax_ceros: element.ax_ceros,
-                    // codigo_stw: element.codigo_stw,
-                    // nombre_cliente: element.nombre_cliente,
-                    // status: element.status ? 'Activo' : 'Inactivo',
-                    // }))
-                    // this.totalRows = res.total
-                    // this.tableTo = res.to
-                    // this.tableFrom = res.from
-                    callback()
+            estudios() {
+                this.$axios.get(`http://localhost/api/estudios`).then(response => {
+                    this.array_estudios = response.data
+                    console.log(this.array_estudios)
+                 
                 }).catch(() => {
-                    //localStorage.removeItem('usuario_cobranza')
+                // this.$toast({
+                // component: ToastificationContent,
+                // props: {
+                //     title: 'Error al cargar datos',
+                //     icon: 'ThumbsDownIcon',
+                //     variant: 'danger',
+                // },
+                // })
                 })
             },
+            
         }
     }
 </script>
